3d73338860fd7aec5a3521b559cde17d4ca8b660
mbilalamjad <79409563+mbilalamjad@users.noreply.github.com>
2024-05-29 18:39:09 -0700

    Initial commit

---
+++ avm/main.tf
@@ -0,0 +1,156 @@
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = ">= 0.3.0"
+}
+
+module "regions" {
+  source  = "Azure/regions/azurerm"
+  version = ">= 0.4.0"
+}
+
+locals {
+  tags = {
+    scenario = "Ubuntu_w_ssh"
+  }
+  test_regions = ["centralus", "eastasia", "eastus2", "westus3"]
+}
+
+resource "random_integer" "region_index" {
+  max = length(local.test_regions) - 1
+  min = 0
+}
+
+resource "random_integer" "zone_index" {
+  max = length(module.regions.regions_by_name[local.test_regions[random_integer.region_index.result]].zones)
+  min = 1
+}
+
+#module "get_valid_sku_for_deployment_region" {
+#  source = "../../modules/sku_selector"
+#  deployment_region = local.test_regions[random_integer.region_index.result]
+#}
+
+resource "azurerm_resource_group" "this_rg" {
+  location = local.test_regions[random_integer.region_index.result]
+  name     = module.naming.resource_group.name_unique
+  tags     = local.tags
+}
+
+resource "azurerm_virtual_network" "this_vnet" {
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this_rg.location
+  name                = module.naming.virtual_network.name_unique
+  resource_group_name = azurerm_resource_group.this_rg.name
+  tags                = local.tags
+}
+
+resource "azurerm_subnet" "this_subnet_1" {
+  address_prefixes     = ["10.0.1.0/24"]
+  name                 = "${module.naming.subnet.name_unique}-1"
+  resource_group_name  = azurerm_resource_group.this_rg.name
+  virtual_network_name = azurerm_virtual_network.this_vnet.name
+}
+
+resource "azurerm_subnet" "this_subnet_2" {
+  address_prefixes     = ["10.0.2.0/24"]
+  name                 = "${module.naming.subnet.name_unique}-2"
+  resource_group_name  = azurerm_resource_group.this_rg.name
+  virtual_network_name = azurerm_virtual_network.this_vnet.name
+}
+
+/* Uncomment this section if you would like to include a bastion resource with this example.
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.this_rg.name
+  virtual_network_name = azurerm_virtual_network.this_vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_public_ip" "bastionpip" {
+  name                = module.naming.public_ip.name_unique
+  location            = azurerm_resource_group.this_rg.location
+  resource_group_name = azurerm_resource_group.this_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = module.naming.bastion_host.name_unique
+  location            = azurerm_resource_group.this_rg.location
+  resource_group_name = azurerm_resource_group.this_rg.name
+
+  ip_configuration {
+    name                 = "${module.naming.bastion_host.name_unique}-ipconf"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastionpip.id
+  }
+}
+*/
+
+
+data "azurerm_client_config" "current" {}
+
+module "avm_res_keyvault_vault" {
+  source              = "Azure/avm-res-keyvault-vault/azurerm"
+  version             = ">= 0.5.0"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  name                = module.naming.key_vault.name_unique
+  resource_group_name = azurerm_resource_group.this_rg.name
+  location            = azurerm_resource_group.this_rg.location
+  network_acls = {
+    default_action = "Allow"
+  }
+
+  role_assignments = {
+    deployment_user_secrets = {
+      role_definition_id_or_name = "Key Vault Secrets Officer"
+      principal_id               = data.azurerm_client_config.current.object_id
+    }
+  }
+
+  wait_for_rbac_before_secret_operations = {
+    create = "60s"
+  }
+
+  tags = local.tags
+}
+
+module "testvm" {
+
+  source = "Azure/avm-res-compute-virtualmachine/azurerm"
+  version = "0.13.1"
+
+  enable_telemetry                       = var.enable_telemetry
+  location                               = azurerm_resource_group.this_rg.location
+  resource_group_name                    = azurerm_resource_group.this_rg.name
+  virtualmachine_os_type                 = "Linux"
+  name                                   = module.naming.virtual_machine.name_unique
+  admin_credential_key_vault_resource_id = module.avm_res_keyvault_vault.resource.id
+  virtualmachine_sku_size                = "Standard_DS2_v2"
+  zone                                   = random_integer.zone_index.result
+
+  source_image_reference = {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  network_interfaces = {
+    network_interface_1 = {
+      name = module.naming.network_interface.name_unique
+      ip_configurations = {
+        ip_configuration_1 = {
+          name                          = "${module.naming.network_interface.name_unique}-ipconfig1"
+          private_ip_subnet_resource_id = azurerm_subnet.this_subnet_1.id
+        }
+      }
+    }
+  }
+
+  tags = local.tags
+
+  depends_on = [
+    module.avm_res_keyvault_vault
+  ]
+}
