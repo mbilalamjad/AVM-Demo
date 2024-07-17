4b55e42a21b1a0a9668ba96735009beed70f0d0d
mbilalamjad <79409563+mbilalamjad@users.noreply.github.com>
2024-07-17 06:52:46 -0700

    Simplify Demo

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.90"
+      version = "~> 3.108"
     }
     random = {
       source  = "hashicorp/random"
